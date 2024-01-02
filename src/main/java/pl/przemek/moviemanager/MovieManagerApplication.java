08548f087197ce8ad368adfcbdf0f0cbbb5dbbea
Przemek <kondaszewskiprz@gmail.com>
2024-01-03 00:33:43 +0100

    Init commit

---
+++ src/main/java/pl/przemek/moviemanager/MovieManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.przemek.moviemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/przemek/moviemanager/MovieManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przemek.moviemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
